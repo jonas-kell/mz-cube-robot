61e9c221fc17db396c201ec22c6d0c6d47f3a42c
Jonas Kell <jonas-kell@web.de>
2024-07-27 23:52:35 +0200

    test: motor consistency

--- nxt-slave/discover.py
+++ nxt-slave/discover.py
@@ -2,6 +2,8 @@
 """NXT-Python tutorial: find the brick."""
 import nxt.locator
 
+# https://ni.srht.site/nxt-python/latest/
+
 # Find a brick.
 with nxt.locator.find() as b:
     # Once found, print its name.
--- nxt-slave/motor.py
+++ nxt-slave/motor.py
@@ -7,6 +7,4 @@ with nxt.locator.find() as b:
     # Get the motor connected to the port A.
     mymotor = b.get_motor(nxt.motor.Port.A)
     # Full circle in one direction.
-    mymotor.turn(25, 360)
-    # Full circle in the opposite direction.
-    mymotor.turn(-25, 360)
+    mymotor.turn(100, 360)
