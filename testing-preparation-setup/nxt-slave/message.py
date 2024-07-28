dc958df97e52f2594b6698c709db1cb53aad54b5
Jonas <j.kell@liwetec.com>
2024-07-28 23:20:50 +0200

    feat: nxt proper target interface

---
+++ testing-preparation-setup/nxt-slave/message.py
@@ -0,0 +1,13 @@
+# https://ni.srht.site/nxt-python/latest/
+
+import nxt.locator
+
+b = nxt.locator.find()
+b.message_write(1, "test".encode("utf-8"))
+
+
+b.message_write(1, "B".encode("utf-8"))
+b.message_write(1, (90).to_bytes(4, "little", signed=True))
+
+b.message_write(1, "A".encode("utf-8"))
+b.message_write(1, (90).to_bytes(4, "little", signed=True))
