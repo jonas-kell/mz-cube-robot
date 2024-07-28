eaa03fc4a71668c68f5ecf6c0d6663e55a64caff
Jonas Kell <jonas-kell@web.de>
2024-07-28 12:12:09 +0200

    fix: tune

--- rpi-server.py
+++ rpi-server.py
@@ -162,7 +162,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                                 nxtPort = nxt.motor.Port.B
 
                             mymotor = NXTbrick.get_motor(nxtPort)
-                            nxt_scaling = 325  # the motor overshoots a quite bit when putting 260, attempt to correct this here
+                            nxt_scaling = 300  # the motor overshoots a quite bit when putting 260, attempt to correct this here
                             mymotor.turn(
                                 -128 if minus else 127,
                                 int(nxt_scaling * percent / 100),
