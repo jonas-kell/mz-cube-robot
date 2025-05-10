ea18c5a724e47245dbf8c3b246fad43a915f30ed
Jonas Kell <jonas-kell@web.de>
2025-05-10 14:55:35 +0200

    feat: add pattern mai mathetag

---
+++ solving-example-code/pattern2.py
@@ -0,0 +1,18 @@
+from move import move
+
+
+move("E", False, "25")
+move("D", True, "25")
+move("E", False, "25")
+move("B", False, "25")
+move("C", True, "25")
+move("B", True, "25")
+move("E", False, "50")
+move("D", False, "50")
+move("B", False, "25")
+move("D", False, "25")
+move("F", True, "25")
+move("A", False, "25")
+move("F", False, "25")
+move("D", False, "50")
+move("B", True, "25")
